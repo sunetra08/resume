e08c3ca075b87643de07ce26e5d028035df13c4e
sunetra08 <76442417+sunetra08@users.noreply.github.com>
2023-04-17 20:45:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'responses.db'
 
+@app.route('/')
+def index():
+    return render_template('contact.html')
 
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
@@ -10,13 +15,13 @@ def submit_form():
     subject = request.form['subject']
     message = request.form['message']
 
-    # Here you can add code to save the form data to a file or a database
-    # For example, you can write the data to a CSV file like this:
-    with open('responses.csv', 'a') as f:
-        f.write(f'{name},{email},{subject},{message}\n')
+    # Save the response to the database
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, subject TEXT, message TEXT)')
+        cursor.execute('INSERT INTO responses (name, email, subject, message) VALUES (?, ?, ?, ?)', (name, email, subject, message))
+        conn.commit()
 
-    return 'Form submitted successfully!'
+    return 'Thank you for your message!'
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __
